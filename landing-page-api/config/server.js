07fd87412702675d85b527bca5e06185f91e2986
Miguel <miguelaugl@outlook.com>
2021-02-06 15:10:15 -0300

    feat: landing page strapi api

---
+++ landing-page-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a33c980607326ff5a4e9c511d92181a8'),
+    },
+  },
+});
---
+++ landing-page-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '234f34b1-d9d4-450e-829d-e3ff3cdb4cdb'
+};
